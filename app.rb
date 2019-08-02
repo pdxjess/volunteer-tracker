ff085fc4a29069b222dd1c13c92890ba2fbbb3c2
Jessica Doss <doss.jessicam@gmail.com>
2019-08-02 16:46:43 -0700

    added some UI

---
+++ app.rb
@@ -0,0 +1,27 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/project')
+require('./lib/volunteer')
+require('pg')
+require('pry')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/') do
+  @projects = Project.all
+  erb(:index)
+end
+
+post('/') do
+  title = params.fetch("title")
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:index)
+end
