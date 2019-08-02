47befec43feb89a54ddd534ee9cd8f62ab5bf0e0
Jessica Doss <doss.jessicam@gmail.com>
2019-08-02 10:06:33 -0700

    add a method to store instances of a project to the database

--- lib/project.rb
+++ lib/project.rb
@@ -10,4 +10,19 @@ class Project
     result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first.fetch('id').to_i
   end
+
+  def ==(project_to_compare)
+    self.title()== project_to_compare.title()
+  end
+
+  def self.all
+    project_list = DB.exec("SELECT * FROM projects;")
+    projects = []
+    project_list.each do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
+  end
 end
