6ac89420f00947f4782457e88ff9e3a4fe217d19
Jessica Doss <doss.jessicam@gmail.com>
2019-08-02 13:11:39 -0700

    add method to find volunteer

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -16,11 +16,24 @@ class Volunteer
     volunteers = []
     volunteer_list.each do |volunteer|
       name = volunteer.fetch("name")
-      id = volunteer.fetch("id")
+      id = volunteer.fetch("id").to_i
       project_id = volunteer.fetch("project_id")
       volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
     end
     volunteers
+  end
+
+  def save
+    result =  DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
+
+  def self.find(id)
+    Volunteer.all.each do |volunteer|
+      if volunteer.id == id
+        return volunteer
+      end
+    end
   end
 
 end
