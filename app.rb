bb45c50617c6a3bc975e5ffc792cd671298da0b9
Jessica Doss <doss.jessicam@gmail.com>
2019-08-02 17:34:44 -0700

    add more UI code

--- app.rb
+++ app.rb
@@ -14,11 +14,6 @@ get('/') do
 end
 
 post('/') do
-  @projects = Project.all
-  erb(:index)
-end
-
-post('/') do
   title = params.fetch("title")
   project = Project.new({:title => title, :id => nil})
   project.save
@@ -32,9 +27,32 @@ get('/projects/:id') do
   erb(:project)
 end
 
+post('/projects/:id') do
+  @project = Project.find(params.fetch(:id).to_i)
+  name = params.fetch("name")
+  project_id = @project.id.to_i
+  volunteer = Volunteer.new({:name => name, :project_id => project_id, :id => nil})
+  volunteer.save
+  @volunteers = @project.volunteers
+  erb(:project)
+end
+
+patch('/projects/:id') do
+  title = params.fetch("title")
+  @project = Project.find(params.fetch(:id).to_i)
+  @project.update({:title => title})
+  erb(:project)
+end
+
 delete('/projects'/:id') do
   @project = Project.find(params.fetch('id').to_i)
   @project.delete
   @projects = Project.all
+  @volunteers = @project.volunteers
   erb(:index)
+end
+
+get('/volunteers/:id') do
+  @volunteer = Volunteer.find(params.fetch(:id).to_i)
+  erb(:volunteer)
 end
